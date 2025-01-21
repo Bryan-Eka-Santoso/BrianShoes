f400ef95ff87f6befbc363d03cb74a2e7ab36de9
Bryan-Eka-Santoso <bryanekasantoso@gmail.com>
2025-01-21 23:09:02 +0700

    added many pages, such as cart, payment details, and more

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,14 @@
-// Pick a card game
-let gameCard = document.getElementById("game-card");
-let gameScore = document.getElementById("game-score");
-let alertGame = document.getElementById("alert-game");
-let cardNumbers = [1, 2, 3];
-let scoreInGame = 0;
-    
-function renderCards() {
-        gameCard.innerHTML = '';
-    
-        cardNumbers = cardNumbers.sort(() => Math.random() - 0.5);
-    
-        for (let i = 0; i < cardNumbers.length; i++) {
-            gameCard.innerHTML += `
-                <div class="col-md-4">
-                    <div class="card card-game bg-light" onclick="checkCard(${cardNumbers[i]})">
-                        <h1> ? </h1>
-                    </div>
-                </div>
-            `;
-        }
-    }
-    
-    function checkCard(value) {
-        if (value === 1) {
-            alertGame.innerHTML = `
-            <div class="alert alert-danger" role="alert">
-                You Lose!! You picked the bomb. Score: ${scoreInGame}
-            </div>
-            `;
-            scoreInGame = 0;
+// For search product
+document.getElementById('searchInput').addEventListener('input', function() {
+    let searchTerm = this.value.toLowerCase();
+    let productCards = document.querySelectorAll('.card-product');
+
+    productCards.forEach(function(card) {
+        let productName = card.querySelector('.card-title').textContent.toLowerCase();
+        if (productName.includes(searchTerm)) {
+            card.style.display = 'block';
         } else {
-            scoreInGame += 10;
-            alertGame.innerHTML = `
-            <div class="alert alert-success" role="alert">
-                You can pick a card again. Score: ${scoreInGame}
-            </div>
-            `;
+            card.style.display = 'none';
         }
-        setTimeout(() => {
-            renderCards();
-        }, 100);
-        }
-        renderCards();
+    });
+});
